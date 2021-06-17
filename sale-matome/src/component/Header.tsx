6a2bd0fe891e450219554052c88ac3706dd1ce8d
aDaikiWatanabe <daiki.watanabe@access-company.com>
2021-06-17 18:13:58 +0900

    create Header

---
+++ sale-matome/src/component/Header.tsx
@@ -0,0 +1,15 @@
+import { AppBar, Box } from '@material-ui/core'
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <AppBar>
+        <Box m={3} fontSize="2.5rem" fontWeight="bold" textAlign="center">
+          セール情報まとめ
+        </Box>
+      </AppBar>
+    </header>
+  )
+}
+
+export default Header
