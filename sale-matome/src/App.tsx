503a0f44c3d11f36f37b20d2a26cf806f4717ebd
aDaikiWatanabe <daiki.watanabe@access-company.com>
2021-06-21 16:21:04 +0900

    Bookコンポーネントの一覧表示

--- sale-matome/src/App.tsx
+++ sale-matome/src/App.tsx
@@ -1,16 +1,7 @@
 import './App.css'
 import Header from './component/Header'
-import Book, { BookType } from './component/Book'
 import BookList from './component/BookList'
 import { Box } from '@material-ui/core'
-
-const book: BookType = {
-  title: 'バカとテストと召喚獣',
-  author: '高橋',
-  price: 200,
-  purchaseLink: 'http://example.com',
-  tag: ['SF', 'ホラー'],
-}
 
 const App: React.FC = () => {
   return (
@@ -18,7 +9,6 @@ const App: React.FC = () => {
       <Header />
       <Box className="Body" mt="8rem" mx="auto">
         <BookList></BookList>
-        <Book book={book} />
       </Box>
     </Box>
   )
--- sale-matome/src/component/BookList.tsx
+++ sale-matome/src/component/BookList.tsx
@@ -1,7 +1,31 @@
-// type Props = {}
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Book, { BookType } from './Book'
 
-const BookList: React.FC = props => {
-  return <></>
+// type Props = {
+// 	filter: string,
+// 	sort: number,
+// 	search: string
+// }
+
+const BookList: React.FC = () => {
+  const [bookList, setBookList] = useState<BookType[]>([])
+  // 最初だけ更新する
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_SERVER}/book`)
+      .then(response => setBookList(response.data.map((x: any) => x.data)))
+      .catch(error => console.log(error))
+  }, [])
+
+  return (
+    <>
+      {bookList.map(book => (
+        // TODO: keyの追加。データに入ってるユニークな値を使いたい。今はダミーからのレスポンスにそれが入ってない。
+        <Book book={book} />
+      ))}
+    </>
+  )
 }
 
 export default BookList
