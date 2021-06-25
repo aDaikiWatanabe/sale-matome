82aeb9c8558b342b2c90f036aa02e92180fbf855
aDaikiWatanabe <daiki.watanabe@access-company.com>
2021-06-25 18:21:37 +0900

    タイトル、作者、タグが長い時に省略するように修正

--- sale-matome/src/component/Book.tsx
+++ sale-matome/src/component/Book.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Card, CardActionArea, CardActions, CardContent, Typography } from '@material-ui/core'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { bool } from 'prop-types'
 import { Book } from '../models/Book'
 // export type BookType = {
 //   title: string
@@ -24,6 +25,12 @@ const useStyles = makeStyles((theme: Theme) =>
 const BookCard: React.FC<Props> = props => {
   const { book } = props
   const classes = useStyles()
+  const author = book.authors[0]
+  const threshold = {
+    title: 20,
+    author: 20,
+    tag: 4,
+  }
 
   return (
     <Box mx="auto" bgcolor="background.paper">
@@ -31,15 +38,21 @@ const BookCard: React.FC<Props> = props => {
         <CardActionArea href={book.purchaseLink}>
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              {book.title}
+              {book.title.length > threshold.title ? book.title.slice(0, threshold.title) + '...' : book.title}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              作者:{`${book.authors[0]}${book.authors.length > 1 ? ' 他' : ''}`}
+              作者:
+              {`${author.length > threshold.author ? author.slice(0, threshold.author) + '...' : author}${
+                book.authors.length > 1 ? ' 他' : ''
+              }`}
               <br />
               価格:{book.price}円
               <br />
               {/* TODO: 各タグにそのタグのフィルタした結果の表示をリンクさせる。 */}
-              ジャンル:{book.tag.join(', ')}
+              ジャンル:
+              {book.tag.length > threshold.tag
+                ? book.tag.slice(0, threshold.tag).join(', ') + ' 他'
+                : book.tag.join(', ')}
             </Typography>
           </CardContent>
         </CardActionArea>
