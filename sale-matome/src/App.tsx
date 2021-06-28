66782f760078a77dacccee54e0b4af3c13f29fd5
aDaikiWatanabe <daiki.watanabe@access-company.com>
2021-06-28 11:58:34 +0900

    アンカーの修正 FilterBoxの追加

--- sale-matome/src/App.tsx
+++ sale-matome/src/App.tsx
@@ -2,17 +2,19 @@ import './App.css'
 import Header from './component/Header'
 import BookList from './component/BookList'
 import SearchBar from './component/SearchBar'
+import FilterBox from './component/FilterBox'
 import { Box, Paper } from '@material-ui/core'
 import BackToTopButton from './component/BackToTopButton'
 
 //TODO: styleで指定する
 const App: React.FC = () => {
   return (
-    <Box className="App" id="back-to-top-anchor">
+    <Box className="App">
       <Header />
       <div id="back-to-top-anchor" />
       <Box className="Body" mt="6rem" mx="auto" p="2rem">
-        <SearchBar />
+        <FilterBox />
+        {/* <SearchBar /> */}
         <Paper style={{ padding: '1rem' }}>
           <BookList />
         </Paper>
